5f0dba32924a2a9fbafb58b446557640a4cea4ef
Jim <jimmontour@gmail.com>
2021-01-13 22:22:09 -0500

    Added todo components outline

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+   render() {
+      return (
+         <div>
+            <header>
+               <h1>To Do List</h1>
+            </header>
+         </div>
+      )
+   }
+}
---
+++ src/components/SubmitTasks.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class SubmitTasks extends Component {
+   render() {
+      return (
+         <div>
+            <input type="text" />
+            <button>Add Task</button>
+         </div>
+      )
+   }
+}
