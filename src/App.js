de036cdec63e9d9c4e1ac63870d212c5d8df7c52
Jim <jimmontour@gmail.com>
2021-01-15 14:21:37 -0500

    Add task submission functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from "react";
-import Header from './components/Header'
-import SubmitTodo from './components/SubmitTodo'
-import TodoList from "./components/TodoList";
+
 class App extends Component {
-   constructor(props) {
-      super(props)
+   constructor() {
+      super();
+
+      this.state = {
+         task:"",
+         tasks:[],
+      };
+   }
+
+   handleChange = (e) => {
+      this.setState({
+         task: e.target.value,
+      });
+   };
+
+   onSubmitTasks = (e) => {
+      e.preventDefault();
+      this.setState({
+         tasks: this.state.tasks.concat(this.state.task),
+         task: "",
+      })
    }
 
    render() {
+      const { task, tasks} = this.state;
+
       return (
-         <div className="container">
-               <Header />
-               <SubmitTodo />
-               <TodoList />
+         <div>
+            <form onSubmit={this.onSubmitTasks}>
+               <label htmlFor="taskInput">Enter task</label>
+               <input
+                  onChange={this.handleChange}
+                  value={this.state.task}
+                  type="text"
+                  id="taskInput"
+               />
+               <button type="submit">
+                  Add Task
+               </button>
+            </form>
          </div>
       );
    }
